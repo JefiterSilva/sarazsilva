0c9afb870996d445caf67c3789942b14f95f924f
Jefiter Silva <jefiterprojetos@gmail.com>
2026-04-14 18:39:18 +0100

    chore: atualiza v2-redesign/jest.config.js

---
+++ v2-redesign/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  testMatch: ['**/?(*.)+(test|spec).ts?(x)'],
+};
